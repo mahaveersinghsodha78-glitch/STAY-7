611016cea5c72c939cd62fca7a7a1f4cd3a6d3f6
mahaveersinghsodha78-glitch <mahaveersinghsodha78@gmail.com>
2025-09-14 16:43:59 +0530

    Update HotelAppApplication.java

--- HotelAppApplication.java
+++ HotelAppApplication.java
@@ -1,11 +1,11 @@
-package com.example.hotelapp;
+package com.stay7.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HotelAppApplication {
+public class Stay7Application {
     public static void main(String[] args) {
-        SpringApplication.run(HotelAppApplication.class, args);
+        SpringApplication.run(Stay7Application.class, args);
     }
 }
