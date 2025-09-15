83abe9ebe3592211d767567826f6eaba2209e877
mahaveersinghsodha78-glitch <mahaveersinghsodha78@gmail.com>
2025-09-15 09:47:17 +0530

    Create HelloController.java

---
+++ rc/main/java/com/stay7/demo/HelloController.java
@@ -0,0 +1,13 @@
+package com.stay7.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "✅ Stay-7 Hotel Booking Backend is LIVE!";
+    }
+}
