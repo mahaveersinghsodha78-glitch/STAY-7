1e4212b5bba02cb7a6d6e798bb949f68449b853b
mahaveersinghsodha78-glitch <mahaveersinghsodha78@gmail.com>
2025-09-15 09:05:42 +0530

    Delete HotelRepository.java
    delete unwanted files

--- HotelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.hotelapp.repository;
-
-import com.example.hotelapp.model.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface HotelRepository extends JpaRepository<Hotel, Long> {
-    List<Hotel> findByCityIgnoreCase(String city);
-}
