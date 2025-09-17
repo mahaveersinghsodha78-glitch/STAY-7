160eb4cb3b9e4cd834812aa665824856cf7a24b3
mahaveersinghsodha78-glitch <mahaveersinghsodha78@gmail.com>
2025-09-17 18:02:16 +0530

    Add files via upload

---
+++ HelloController.java
@@ -0,0 +1,13 @@
+package com.stay7.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello Mahaveer Singh! ✅ Backend is working fine.";
+    }
+}
---
+++ Stay7Application.java
@@ -0,0 +1,11 @@
+package com.stay7.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stay7Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Stay7Application.class, args);
+    }
+}
