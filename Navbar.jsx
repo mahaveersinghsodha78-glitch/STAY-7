db6fc084a1b41f66c139f6d99e20408297f48671
mahaveersinghsodha78-glitch <mahaveersinghsodha78@gmail.com>
2025-09-18 01:45:41 +0530

    Delete Navbar.jsx

--- Navbar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Navbar() {
-  return (
-    <nav className="bg-blue-600 text-white p-4">
-      <h1 className="text-xl font-bold">Stay7 Hotels</h1>
-    </nav>
-  )
-}
