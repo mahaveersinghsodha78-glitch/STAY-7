f9f677a96f34609a6334beb38610d96407188127
mahaveersinghsodha78-glitch <mahaveersinghsodha78@gmail.com>
2025-09-21 01:23:55 +0530

    Delete Footer.jsx

--- Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-  return (
-    <footer className="bg-gray-800 text-white text-center p-4">
-      <p>© 2025 Stay7 Hotels. All rights reserved.</p>
-    </footer>
-  )
-}
