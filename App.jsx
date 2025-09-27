a7e8140447d1e65ba5e27447724f80b392efb4c0
mahaveersinghsodha78-glitch <mahaveersinghsodha78@gmail.com>
2025-09-27 16:57:28 +0530

    Delete App.jsx

--- App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="flex items-center justify-center h-screen bg-gray-100 text-2xl font-bold">
-      Stay7 Hotel Booking App Working 🚀
-    </div>
-  )
-}
-
-export default App
